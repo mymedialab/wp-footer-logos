3258fe4caa49848884114171d32ea4d16138be72
Barry Rhodes <barry@mymedialab.co.uk>
2015-06-08 22:01:15 +0100

    front end outputs images.

--- widget-public.php
+++ widget-public.php
@@ -1,5 +1,31 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
-<?php foreach ($logos as $logo) : ?>
-    <?php var_dump($logo); ?>
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+     // Exit if accessed directly
+    exit;
+}
+ if ( !count($logos) ) {
+    // output nothing if no logos are setup
+    return;
+ } ?>
 
-<?php endforeach; ?>
+<ul class="footer-logos">
+
+    <?php foreach ($logos as $logo) : ?>
+
+        <li class="footer-logos__item">
+
+            <?php if ($logo['link']) : ?>
+                <a class="footer-logos__link" href="<?= $logo['link'] ?>">
+            <?php endif; ?>
+
+            <?= wp_get_attachment_image($logo['image_id'], 'full', array('class' => 'footer-logos__img')); ?>
+
+            <?php if ($logo['link']) : ?>
+                </a>
+            <?php endif; ?>
+
+        </li>
+
+    <?php endforeach; ?>
+
+</ul>
